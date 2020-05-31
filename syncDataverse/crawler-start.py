11029a538570ab563ee4ddfd5ed6dc0fd1906a42
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2020-05-31 12:41:59 +0200

    Dataverse crawler script added

---
+++ syncDataverse/crawler-start.py
@@ -0,0 +1,24 @@
+from config import DV_ALIAS, BASE_URL, API_TOKEN, REPO, GITHUB_TOKEN, PARSABLE_EXTENSIONS, gitroot, gituserroot, gitblob, gitmaster
+from syncdataverse import DataverseData
+from github import Github
+from pyDataverse.api import Api,NativeApi
+from pyDataverse.models import Datafile, Dataset
+
+repos = []
+q = 'covid'
+dvn = DataverseData(REPO)
+
+for repo in dvn.githubsearch(q)[0:10]:
+    try:
+        print(repo.full_name)
+        if not dvn.if_exist(repo.full_name):
+            repos.append(repo.full_name)
+    except:
+        print("something wrong with %s" % repo.full_name)
+
+for thisrepo in repos:
+    print(thisrepo)
+    dvn = DataverseData(thisrepo, True)
+    print(dvn.collect_urls())
+    dvn.datasync()
+
